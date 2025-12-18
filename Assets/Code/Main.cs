b9147d26ba43226849c40809635945dfe9aff630
Vladyslav-Urdenko <akashiigame714@gmail.com>
2025-12-18 18:38:06 +0200

    Ready Main Menu

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class Main : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider audioSlider;  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MusicManager.Instance.PlayMusic("Magic");
+        LoadVolume();
 
     }
     public void Play() { 
@@ -16,6 +22,30 @@ public class Main : MonoBehaviour
         Application.Quit();
     }   
     // Update is called once per frame
+    
+    public void UploadMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UploadAudioVolume(float volume) {         
+        audioMixer.SetFloat("SoundsVolume", volume);
+    }   
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SoundsVolume", out float soundsVolume);
+        PlayerPrefs.SetFloat("SoundsVolume", soundsVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        audioSlider.value = PlayerPrefs.GetFloat("SoundsVolume");
+    }
     void Update()
     {
         
