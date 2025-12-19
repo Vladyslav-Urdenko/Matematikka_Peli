e8baa7412fe74b19084a00e10cb4d8d3a16a46ae
EgoisticHero <myronov.n@gmail.com>
2025-12-19 15:20:27 +0200

    Quiz logic
    Added a quiz level logic

---
+++ Assets/Editor/CSVtoSO.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CVStoSO
+{
+    private static string questionsCSVPath = "/Editor/CSVs/Questions.csv";
+    private static string questionsPath = "Assets/Resources/Questions/";
+    private static int numberOfAnswers = 4;
+
+    [MenuItem("Utilities/Generate Questions")]
+    public static void GeneratePhrases()
+    {
+
+        string[] allLines = File.ReadAllLines(Application.dataPath + questionsCSVPath);
+
+        foreach (string s in allLines)
+        {
+            string[] splitData = s.Split(',');
+
+            QuestionData questionData = ScriptableObject.CreateInstance<QuestionData>();
+            questionData.question = splitData[0]; 
+            questionData.category = splitData[1];
+
+            questionData.answers = new string[4];
+
+            if (!Directory.Exists(questionsPath))
+            {
+                Directory.CreateDirectory(questionsPath);
+            }
+
+            for (int i = 0; i < numberOfAnswers; i++)
+            {
+                questionData.answers[i] = splitData[2 + i];
+            }
+
+            if (questionData.question.Contains("?"))
+            {
+                questionData.name = questionData.question.Remove(questionData.question.IndexOf("?"));
+            }
+            else 
+            {
+                questionData.name = questionData.question;
+            }
+            AssetDatabase.CreateAsset(questionData, $"{questionsPath}/{questionData.name}.asset");
+        }
+
+        AssetDatabase.SaveAssets();
+
+        Debug.Log($"Generated Questions");
+    }
+}
